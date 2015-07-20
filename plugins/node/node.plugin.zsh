11429553970a25c6962156d28877531073129135
solarmicrobe <solarmicrobe@users.noreply.github.com>
2015-07-20 14:20:29 -0500

    Update plugins to support cygwin
    Added cygwin open command

--- plugins/node/node.plugin.zsh
+++ plugins/node/node.plugin.zsh
@@ -5,6 +5,8 @@ function node-docs {
   local open_cmd
   if [[ "$OSTYPE" = darwin* ]]; then
     open_cmd='open'
+  elif [[ "$OSTYPE" = 'cygwin' ]]; then
+    open_cmd='cygstart'
   else
     open_cmd='xdg-open'
   fi
